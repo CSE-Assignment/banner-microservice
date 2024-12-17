90dbe010048f40a515a21fb3aaeb680b4689eb13
edaklg <69681428+edaklg@users.noreply.github.com>
2024-12-17 13:52:10 +0100

    Update test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,16 +5,18 @@ from generated import banner_service_pb2, banner_service_pb2_grpc
 pytestmark = pytest.mark.integration
 
 def test_banner_service():
-    """Test the deployed banner service."""
-    channel = grpc.insecure_channel("bannerservice:51234")
+    """Test the locally running banner service."""
+    # Connect to the local server
+    channel = grpc.insecure_channel("localhost:51234")
     client = banner_service_pb2_grpc.BannerServiceStub(channel)
 
     # Send a request
     request = banner_service_pb2.GetCurrentBannerRequest(location="US")
     response = client.GetCurrentBanner(request)
 
-    # Validate
+    # Validate the response
     print(f"Title: {response.title}")
     print(f"Description: {response.description}")
     assert response.title
     assert response.description
+
